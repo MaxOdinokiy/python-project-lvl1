291f7d4cc8fbfcf993c5fff9afcd106f657b94df
Max Odinokiy <max.odinokiy@gmail.com>
2022-08-01 21:47:47 +0300

    Trying to fix 5

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,7 +7,7 @@ LENGTH = 10
 def get_question_answer():
     start = random.randint(0, 20)
     step = random.randint(-5, 5)
-    progression = [str(start + step*i) for i in range(1, LENGTH + 1)]
+    progression = [str(start + step * i) for i in range(1, LENGTH + 1)]
     missing_index = random.randint(0, LENGTH - 1)
     right_answer = progression[missing_index]
     progression[missing_index] = '..'
