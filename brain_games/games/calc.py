4c9421caa426dff9adc18f9520ae6aff84566d07
Max Odinokiy <max.odinokiy@gmail.com>
2022-08-01 21:44:25 +0300

    Trying to fix 4

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -14,6 +14,6 @@ def get_question_answer():
     x1 = random.randint(0, 25)
     x2 = random.randint(0, 25)
     sign, operation = random.choice(OPERATIONS)
-    question = '{}{}{}'.format(x1, sign, x2)
+    question = '{} {} {}'.format(x1, sign, x2)
     right_answer = str(operation(x1, x2))
     return question, right_answer
